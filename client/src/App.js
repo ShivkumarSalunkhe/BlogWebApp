164d451fe6bb4336b66db67dd0460f01a28fe2a7
Shivkumar <shivkumarsalunkhe50gmail.com>
2023-01-08 13:03:18 +0530

    Added components login & signup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Login from './Components/Accounts/Login';
+import Signup from './Components/Accounts/Signup';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Signup/>
+      hello
+    </div>
+  );
+}
+
+export default App;
