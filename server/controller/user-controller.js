d8404a9ec1b4aec7fc869ade5308fc60d36c45e9
Shivkumar <shivkumarsalunkhe50gmail.com>
2023-01-09 00:25:13 +0530

    Added bycrypt for hashing user password

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,18 +1,27 @@
 import User from "../model/user.js"
+import bcrypt from 'bcrypt'
 
-export const signupUser= async (req, res)=>{
+export const signupUser = async (req, res) => {
     try {
-        const user = req.body
+        // const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(req.body.password, 10);
+        
+        const user = {
+            username: req.body.username,
+            name: req.body.name,
+            password: hashPassword
+        }
+
         const newUser = new User(user);
         await newUser.save();
         return res.status(200)
-        .json({
-            msg:"Signup Successful"
-        })
+            .json({
+                msg: "Signup Successful"
+            })
     } catch (error) {
         return res.status(500)
-        .jsion({
-            msg: "Error while signup user"
-        })
+            .json({
+                msg: "Error while signup user"
+            })
     }
 }
